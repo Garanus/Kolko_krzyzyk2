b198566f98ec3614d8eebaaf9ebd688878d23cf2
Michalus21 <mimonowi@wp.pl>
2019-01-03 18:22:01 +0100

    Dodaj pliki projektów.

---
+++ DXApplication2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DXApplication2
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        bool turn = true;// true = X turn; false = Y turn
+        int turn_count = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Wynik przycisku o aplikacji
+        private void howToPlayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Zasady klasycznej gry w kółko i krzyżyk. Pozdrawiam Michał Nowicki");
+        }
+        //wyjście z aplikacji
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //efekt zmiany znaku z X na O po wykonaniu kliknięcia na pole
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+        //Sprawdzenie możliwych opcji zaznaczenia w grze
+        private void checkForWinner()
+        {
+            bool there_is_a_winner = false;
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                there_is_a_winner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                there_is_a_winner = true;
+
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+            //decyzja kto wygrał
+            if (there_is_a_winner)
+            {
+                disableButtons();
+
+                String winner = "";
+                if (turn)
+                    winner = "O";
+                else
+                    winner = "X";
+
+                MessageBox.Show(winner + " wygrywa!", "Gratulacje!");
+            }
+            else
+            {
+                if(turn_count == 9)
+                    MessageBox.Show("Brak zwycięzcy!");
+            }
+        }
+        //wyłączanie przycisku po wykonaniu akcji
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Component c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+        //Zerowanie pól po wybraniu nowej gry
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+            try
+            {
+                foreach (Component c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = " ";
+                }
+            }
+            catch { }
+        }
+    }
+}
